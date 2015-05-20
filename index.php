09456d81be810d3d99531abaa3309cb6744ff74d
Joao Pinto <jfd.pinto@campus.fct.unl.pt>
2015-05-20 12:50:39 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<table>
+	<tr>
+		<td>1</td>
+		<td>2</td>
+		<td>3</td>
+	</tr>
+	<tr>
+		<td>4</td>
+		<td>5</td>
+		<td>6</td>
+	</tr>
+</table>
+</body>
+</html>
