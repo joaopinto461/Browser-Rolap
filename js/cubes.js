c09e094d404151ec0b3fce0b4b11d7e973ad1552
Gonçalo Dias da Silva <gopdiasdasilva@gmail.com>
2015-06-13 23:20:13 +0100

    Adding and removing dimensions.

--- js/cubes.js
+++ js/cubes.js
@@ -1,21 +1,9 @@
 $(document).ready(function()
 {	   
-	/* Receive measures from server in array */
-	var measures = ["Units Ordered", "Sales", "Cost"];
+	
 
-	for(var x in measures)
-	{
-		var m = measures[x];
-		$('.measures_list').append('<li class="measure" id="' + m + '">' + '<i class="glyphicon glyphicon-tasks"></i> ' + m + '</li>');
-	}
-
-	var dimensions = ["Product Dimension", "Time Dimension", "Store Dimension"];
-
-	for(var x in dimensions)
-	{
-		var d = dimensions[x];
-		$('.dimensions_list').append('<li class="dimension" id="' + d + '">' + '<i class="glyphicon glyphicon-list"></i> ' + d + '</li>');
-	}
+	fillDimensions();
+	fillMeasures();
 
 	$('.center_col').droppable
 	(
@@ -35,19 +23,8 @@ $(document).ready(function()
 		        {
 		        	var nd = ui.draggable.attr('id');
 		        	active_dimensions[nd] = nd;
-		        	$('.active_dimensions_list').html("");
-		        	
-		        	for(var x in active_dimensions)
-					{
-						var ad = active_dimensions[x];			
-						$('.active_dimensions_list').append('<li class="active_dimension" id="' + ad + '">' + '<i class="glyphicon glyphicon-remove"></i> ' + ad + '</li>');
-					}
-		        }
-
-		        // for(var b in active_measures)
-		        // {
-		        // 	console.log(active_measures[b]);
-		        // }		        
+		        	fillActiveDimensions();		        	
+		        }	        
 		    }
 		}
 	);
@@ -58,11 +35,51 @@ $(document).ready(function()
 		    revert: true,
 		    helper: "clone"		    
 		}
-	)	   
+	);
+	
 });
 
 var active_measures = {};
 var active_dimensions = {};
 
+var dimensions = ["Product Dimension", "Time Dimension", "Store Dimension"];
+var measures = ["Units Ordered", "Sales", "Cost"];	
 
+function deleteElem(clicked_id)
+{
+	delete active_dimensions[clicked_id];
+	fillActiveDimensions();
+}
 
+function fillDimensions()
+{
+	for(var x in dimensions)
+	{
+		var d = dimensions[x];
+		$('.dimensions_list').append('<li class="dimension" id="' + d + '">' + '<i class="glyphicon glyphicon-list"></i> ' + d + '</li>');
+	}
+}
+
+function fillActiveDimensions()
+{
+	$('.active_dimensions_list').html("");		        	
+	for(var x in active_dimensions)
+	{
+		var ad = active_dimensions[x];			
+		$('.active_dimensions_list').append('<li class="active_dimension" id="' + ad + '">' + '<i onClick="deleteElem(this.id)" class="active_dimension glyphicon glyphicon-remove" id="' + ad + '"></i> ' + ad + '</li>');
+	}
+}
+
+function fillMeasures()
+{
+	for(var x in measures)
+	{
+		var m = measures[x];
+		$('.measures_list').append('<li class="measure" id="' + m + '">' + '<i class="glyphicon glyphicon-tasks"></i> ' + m + '</li>');
+	}
+}
+
+function fillActiveMeasures()
+{
+	
+}
