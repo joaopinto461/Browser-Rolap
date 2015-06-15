9b84dcff896d0e9abac6b6849a051feb5e7eef74
Gonçalo Dias da Silva <gopdiasdasilva@gmail.com>
2015-06-15 12:20:12 +0100

    Using our metadata

--- main.php
+++ main.php
@@ -1,49 +1,51 @@
 <?
-	// $myxml = simplexml_load_file('xml/teste.xml');
-	// #$myxml = simplexml_load_file('xml/exemplo_DW.xml');
+    function libxml_display_error($error)
+    {
+        $return = "<br/>\n";
+        switch ($error->level)
+        {
+            case LIBXML_ERR_WARNING:
+                $return .= "<b>Warning $error->code</b>: ";
+                break;
+            case LIBXML_ERR_ERROR:
+                $return .= "<b>Error $error->code</b>: ";
+                break;
+            case LIBXML_ERR_FATAL:
+                $return .= "<b>Fatal Error $error->code</b>: ";
+                break;
+        }
+        $return .= trim($error->message);
+        if ($error->file)
+        {
+            $return .=    " in <b>$error->file</b>";
+        }
+
+        $return .= " on line <b>$error->line</b>\n";
+
+        return $return;
+    }
+
+    function libxml_display_errors()
+    {
+        $errors = libxml_get_errors();
+        foreach ($errors as $error)
+        {
+            print libxml_display_error($error);
+        }
+        libxml_clear_errors();
+    }
+
+	libxml_use_internal_errors(true);
+	$doc = new DOMDocument;
+	$doc->load('xml/metadataDW.xml');
+
+	if (!$doc->schemaValidate('xml/metadataDW.xsd'))
+    {
+	    print '<b>DOMDocument::schemaValidate() ::Generated Errors!</b>';
+	    libxml_display_errors();
+	}
 	
-	// echo $myxml->key;
-// function libxml_display_error($error)
-// {
-//     $return = "<br/>\n";
-//     switch ($error->level) {
-//         case LIBXML_ERR_WARNING:
-//             $return .= "<b>Warning $error->code</b>: ";
-//             break;
-//         case LIBXML_ERR_ERROR:
-//             $return .= "<b>Error $error->code</b>: ";
-//             break;
-//         case LIBXML_ERR_FATAL:
-//             $return .= "<b>Fatal Error $error->code</b>: ";
-//             break;
-//     }
-//     $return .= trim($error->message);
-//     if ($error->file) {
-//         $return .=    " in <b>$error->file</b>";
-//     }
-//     $return .= " on line <b>$error->line</b>\n";
-
-//     return $return;
-// }
-
-// function libxml_display_errors() {
-//     $errors = libxml_get_errors();
-//     foreach ($errors as $error) {
-//         print libxml_display_error($error);
-//     }
-//     libxml_clear_errors();
-// }
-
-// 	libxml_use_internal_errors(true);
-// 	$doc = new DOMDocument;
-// 	$doc->load('xml/metadata.xml');
-
-// 	if (!$doc->schemaValidate('xml/metadata.xsd')) {
-// 	    print '<b>DOMDocument::schemaValidate() ::Generated Errors!</b>';
-// 	    libxml_display_errors();
-// 	}
-// 	echo $doc->getElementById('tab_account')->getAttribute('name');;
-
+    // echo $doc->getElementById('table_sales_fact_1997_column_product_id')->getAttribute('type');
 
 	if(isset($_GET['cubes']))
 	{
